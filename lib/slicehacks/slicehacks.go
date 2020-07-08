bf945b6b79efe01a6773845bb034fa09df71c2d6
Hari Bhaskaran <hbhaskaran@gmail.com>
2020-07-08 15:52:40 -0700

    Update LICENSE from MIT to Apache 2.0 to align with other projects

--- lib/slicehacks/slicehacks.go
+++ lib/slicehacks/slicehacks.go
@@ -1,12 +1,13 @@
 /*
-Copyright 2020 Adobe
-All Rights Reserved.
+Copyright 2020 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
 
-NOTICE: Adobe permits you to use, modify, and distribute this file in
-accordance with the terms of the Adobe license agreement accompanying
-it. If you have received this file from a source other than Adobe,
-then your use, modification, or distribution of it requires the prior
-written permission of Adobe.
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
 */
 
 package slicehacks
